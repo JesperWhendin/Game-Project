773b8e46e43d932689e53ca6c8ecbc6bd44dbf93
JesperWhendin <143404976+JesperWhendin@users.noreply.github.com>
2024-06-20 13:56:11 +0200

    Add project files.

---
+++ Game-Project.Common/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Game_Project.Common;
+
+public interface IRepository<TEntity, TId> where TEntity : IEntity<TId>
+{
+    Task<TEntity> GetByIdAsync(TId id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TId id);
+}
